b10f14059eba14806447e6cec643a6e499afc8b0
Konstantin <drozhzhins93@rambler.ru>
2024-06-06 14:59:22 +0300

    update elements tests added tests: test_web_table_add_person test_web_table_search_person

--- data/data.py
+++ data/data.py
@@ -3,6 +3,11 @@ from dataclasses import dataclass
 
 @dataclass
 class Person:
+    firstname: str = None
+    lastname: str = None
+    age: int = None
+    salary: int = None
+    department: str = None
     full_name: str = None
     email: str = None
     current_address: str = None
--- generator/generator.py
+++ generator/generator.py
@@ -1,3 +1,5 @@
+import random
+
 from data.data import Person
 from faker import Faker
 
@@ -6,6 +8,11 @@ Faker.seed()
 def generated_person():
     yield Person(
         full_name=faker_ru.first_name() + " " + faker_ru.last_name() + " " + faker_ru.middle_name(),
+        firstname=faker_ru.first_name(),
+        lastname=faker_ru.last_name(),
+        age=random.randint(10,88),
+        department=faker_ru.job(),
+        salary=random.randint(10000, 100000),
         email=faker_ru.email(),
         current_address=faker_ru.address(),
         permanent_address=faker_ru.address(),
